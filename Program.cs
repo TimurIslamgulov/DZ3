27c54da886d8c5684989be986ef295c665f63079
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-09-01 18:16:35 +0500

    Апгрейд (выводит теперь квадра)т

--- Program.cs
+++ Program.cs
@@ -40,31 +40,31 @@ StepNumber(n, arr);
 //Вывод  от 1 до 5
 Console.SetCursorPosition(0, 5);
 Console.Write(arr[0] + " ");
-Console.SetCursorPosition(4, 5);
+Console.SetCursorPosition(5, 5);
 Console.Write(arr[1] + " ");
-Console.SetCursorPosition(8, 5);
+Console.SetCursorPosition(10, 5);
 Console.Write(arr[2] + " ");
-Console.SetCursorPosition(12, 5);
+Console.SetCursorPosition(15, 5);
 Console.Write(arr[3] + " ");
-Console.SetCursorPosition(16, 5);
+Console.SetCursorPosition(20, 5);
 Console.Write(arr[4] + " ");
 
 //Вывод  от 5 до 9
-Console.SetCursorPosition(16, 7);
+Console.SetCursorPosition(20, 7);
 Console.Write(arr[5] + " ");
-Console.SetCursorPosition(16, 9);
+Console.SetCursorPosition(20, 9);
 Console.Write(arr[6] + " ");
-Console.SetCursorPosition(16, 11);
+Console.SetCursorPosition(20, 11);
 Console.Write(arr[7] + " ");
-Console.SetCursorPosition(16, 13);
+Console.SetCursorPosition(20, 13);
 Console.Write(arr[8] + " ");
 
 //Вывод  от 9 до 13
-Console.SetCursorPosition(12, 13);
+Console.SetCursorPosition(15, 13);
 Console.Write(arr[9] + " ");
-Console.SetCursorPosition(8, 13);
+Console.SetCursorPosition(10, 13);
 Console.Write(arr[10] + " ");
-Console.SetCursorPosition(4, 13);
+Console.SetCursorPosition(5, 13);
 Console.Write(arr[11] + " ");
 Console.SetCursorPosition(0, 13);
 Console.Write(arr[12] + " ");
@@ -78,31 +78,31 @@ Console.SetCursorPosition(0, 7);
 Console.Write(arr[15] + " ");
 
 //Вывод  от 16 до 19
-Console.SetCursorPosition(4, 7);
+Console.SetCursorPosition(5, 7);
 Console.Write(arr[16] + " ");
-Console.SetCursorPosition(8, 7);
+Console.SetCursorPosition(10, 7);
 Console.Write(arr[17] + " ");
-Console.SetCursorPosition(12, 7);
+Console.SetCursorPosition(15, 7);
 Console.Write(arr[18] + " ");
 
 //Вывод  от 19 до 21
-Console.SetCursorPosition(12, 9);
+Console.SetCursorPosition(15, 9);
 Console.Write(arr[19] + " ");
-Console.SetCursorPosition(12, 11);
+Console.SetCursorPosition(15, 11);
 Console.Write(arr[20] + " ");
 
 //Вывод  от 21 до 23
-Console.SetCursorPosition(8, 11);
+Console.SetCursorPosition(10, 11);
 Console.Write(arr[21] + " ");
-Console.SetCursorPosition(4, 11);
+Console.SetCursorPosition(5, 11);
 Console.Write(arr[22] + " ");
 
 //Вывод  от 23 до 24
-Console.SetCursorPosition(4, 9);
+Console.SetCursorPosition(5, 9);
 Console.Write(arr[23] + " ");
 
 //Вывод  от 24 до 25
-Console.SetCursorPosition(8, 9);
+Console.SetCursorPosition(10, 9);
 Console.Write(arr[24] + " ");
 
 
