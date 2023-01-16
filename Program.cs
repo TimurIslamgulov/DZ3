72daf98a7a564238120bd7989a6120feba43576a
Good1104 <ilam43950@gmail.com>
2023-01-16 22:07:54 +0300

    sorr

--- Program.cs
+++ Program.cs
@@ -1,56 +1,52 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Обращаться к числу как к строке нельзя.
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// 645 -> 5
+// 3, 5 -> 243 (3⁵)
 
-// 78 -> третьей цифры нет
-
-// 32679 -> 6
-
-
-// // }
-// Console.WriteLine("введите число");
-// int x = Convert.ToInt32(Console.ReadLine());
-// if (x < 100) 
+// 2, 4 -> 16
+// Console.WriteLine("введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int Quter = Convert.ToInt32(Math.Pow(A,B));
 // {
-// Console.WriteLine("у числа нет 3ий цифры");}
-// else {
-//     do
-//     {
-//      x = x / 10;}
-//      while(x > 999);
+// Console.WriteLine($" number{Quter}");
+// }
 
-// int b = x % 10;
-// Console.WriteLine($"3е число{b}");}
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
 
+// 452 -> 11
 
+// 82 -> 10
 
-// Задача необязательная 2: по желанию идет за 2 необязательных
+// 9012 -> 12
+// Console.WriteLine("введите число A");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int i = 1;
+// while(i <= A)
+// {
+//     sum = sum + ( A % 10);
+//     A = A / 10;
+//     }
+// Console.Write($"sum = {sum}");
+// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры 
+// и далее выводит массив на экран в одну строку.
 
-// В институте биоинформатики по офису передвигается робот. Недавно студенты из группы программистов написали для него программу, по которой робот, когда заходит в комнату, считает количество программистов в ней и произносит его вслух: "n программистов".
-
-// Для того, чтобы это звучало правильно, для каждого n нужно использовать верное окончание слова.
-
-// Напишите программу, считывающую с пользовательского ввода целое число n (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом "программист", для того, чтобы робот мог нормально общаться с людьми, например: 1 программист, 2 программиста, 5 программистов.
-
-// В комнате может быть очень много программистов. Проверьте, что ваша программа правильно обработает все случаи, как минимум до 100 человек.
-
-Console.WriteLine("введите число");
-int x = Convert.ToInt32(Console.ReadLine());
-int y = x % 100;
-int z = x % 10;
-
-if (y > 10&& y< 15)
-{
-    
-    Console.WriteLine($"{x} программистов");
-}
-    else if (z > 0 && z < 5)
-    { Console.WriteLine($"{x} программиста");
-    }
-else
-    {
-    
-Console.WriteLine($"{x} программистов");
-    }
-    // УРА!!!
-
+// int[]Array = new int[8];
+// Console.WriteLine("введите 1 число");
+// Array[0] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 2 число");
+// Array[1] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 3 число");
+// Array[2] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 4 число");
+// Array[3] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 5 число");
+// Array[4] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 6 число");
+// Array[5] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 7 число");
+// Array[6] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите 8 число");
+// Array[7] = Convert.ToInt32(Console.ReadLine());
+// foreach (int item in Array) Console.Write(item);
